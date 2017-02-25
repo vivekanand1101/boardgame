5d5435c78ede10df2a60f7f7eac90ec2c865e926
Vivek Anand <vivekanand1101@gmail.com>
2017-02-25 13:32:46 +0530

    Clean the play command UI
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- bdgame/commands/play.py
+++ bdgame/commands/play.py
@@ -4,44 +4,13 @@
 import click
 
 from bdgame.app import app
-from bdgame.utils import get_item, check_grid_sane, get_grid
+from bdgame.utils import load_game_conf
 from bdgame.exceptions import ItemNotFound, BDException
 
 @app.command()
 def play():
     ''' Play the game after loading configuration from .bdgame '''
 
-    nplayers = get_item('nplayers')
-    if not nplayers:
-        raise ItemNotFound('Number of players(nplayers) not found in .bdgame')
-    nplayers = int(nplayers)
-
-    players = get_item('players')
-    if not players:
-        raise ItemNotFound('Player names (players) not found in .bdgame')
-
-    players = players.split(',')
-    if len(players) != nplayers:
-        raise BDException('Names of all the players aren\'t present in .bdgame file')
-
-    gsize = get_item('gsize')
-    if not gsize:
-        raise ItemNotFound('Grid size (gsize) is absent in .bdgame')
-
-    if len(gsize.split(' ')) != 2:
-        raise BDException('Grid size format is wrong.')
-
-    gsize = gsize.strip().split(' ')
-    glen, gbred = gsize[0], gsize[1]
-
-    grid = get_item('grid')
-    if not grid:
-        raise ItemNotFound('Grid is absent in .bdgame')
-
-    sane = check_grid_sane(grid, glen, gbred)
-    if not sane:
-        click.echo('Grid does not match with grid size, check in config file '
-                   ' or make the game again')
-
-    # Get the grid in form of 2D array
-    grid = get_grid(grid)
+    # Loads the config and returns a dict
+    # containing 'nplayers', 'players', 'grid' as keys
+    items = load_game_conf()
--- bdgame/utils/__init__.py
+++ bdgame/utils/__init__.py
@@ -79,3 +79,44 @@ def get_grid(grid):
         row = row.split()
         grid_copy.append(row)
     return grid_copy
+
+
+def load_game_conf():
+    ''' Method which returns all the items required for the game '''
+
+    nplayers = get_item('nplayers')
+    if not nplayers:
+        raise ItemNotFound('Number of players(nplayers) not found in .bdgame')
+    nplayers = int(nplayers)
+
+    players = get_item('players')
+    if not players:
+        raise ItemNotFound('Player names (players) not found in .bdgame')
+
+    players = players.split(',')
+    if len(players) != nplayers:
+        raise BDException('Names of all the players aren\'t present in .bdgame file')
+
+    gsize = get_item('gsize')
+    if not gsize:
+        raise ItemNotFound('Grid size (gsize) is absent in .bdgame')
+
+    if len(gsize.split(' ')) != 2:
+        raise BDException('Grid size format is wrong.')
+
+    gsize = gsize.strip().split(' ')
+    glen, gbred = gsize[0], gsize[1]
+
+    grid = get_item('grid')
+    if not grid:
+        raise ItemNotFound('Grid is absent in .bdgame')
+
+    sane = check_grid_sane(grid, glen, gbred)
+    if not sane:
+        click.echo('Grid does not match with grid size, check in config file '
+                   ' or make the game again')
+
+    # Get the grid in form of 2D array
+    grid = get_grid(grid)
+
+    return {'nplayers': nplayers, 'players': players, 'grid': grid}
